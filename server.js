d3ae8dfcef0ceefcdd7d7c5c910b0f7d670582c4
mkotte <74936870+mkotte@users.noreply.github.com>
2021-04-15 17:33:38 -0400

    html routes working, api not yet

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
+const dbInfo = require('./Develop/db/db.json')
 
 // Express Configurations
 const app = express();
@@ -11,7 +11,18 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/notes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/index.html')));
 
-app.get('api/notes', (req,res) => )
+app.get('api/notes', (req, res) => res.json(dbInfo));
+app.post('api/notes', (req, res) => {
+    const newNote = req.body;
+
+    
+    
+    
+})
+
+
+// listener 
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
