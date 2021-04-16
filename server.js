81b01c013e52fffcdf54a675d980e4ab165e65bd
mkotte <74936870+mkotte@users.noreply.github.com>
2021-04-16 00:09:30 -0400

    switched from readfilesync to readfile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
+// const dbInfo = require('./Develop/db/db.json')
 
 // Express Configurations
 const app = express();
@@ -14,19 +14,22 @@ app.use(express.json());
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/notes.html')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'Develop/public/index.html')));
 
-
+// app.get('/api/notes', (req, res) => {
+//     res.json(JSON.parse(dbInfo))
+// });
 
 app.get('/api/notes', (req, res) => {
-    let dbInfo = fs.readFileSync('Develop/db/db.json', 'utf8')    
-    res.json(JSON.parse(dbInfo))
+    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+        if(err){res.status(400).json(err) } 
+        res.status(200).json(JSON.parse(data))
+    });     
 });
 
-app.post('/api/notes', (req, res) => {
-    let dbInfo = fs.readFileSync('Develop/db/db.json', 'utf8')
-    
-    
-    
-})
+// app.post('/api/notes', (req, res) => {
+//     let dbInfo = fs.readFileSync('Develop/db/db.json', 'utf8');
+//     dbInfo.push(req.body);
+//     res.json(true);
+// })
 
 
 // listener 
