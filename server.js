7986a6b99a077d9c4160029abc1adc826e273cae
mkotte <74936870+mkotte@users.noreply.github.com>
2021-04-19 22:29:07 -0400

    restructured files + made changes to server.js
    realized that my assets were not in the public branch therefore not connected to the server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 //
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 //
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
@@ -27,18 +27,20 @@ app.get('/api/notes', (req, res) => {
 
 //
 app.post('/api/notes', (req, res) => {
-    const newNote = {
-        title: req.body.title,
-        text: req.body.text,
-      };
+    
     fs.readFile('db/db.json', 'utf8', (err, data) => {
         if(err){ res.status(400).json(err) };
         const dbInfo = JSON.parse(data);
+        const newNote = {
+                title: req.body.title,
+                text: req.body.text,
+                id: dbInfo.length + 1
+        };
         dbInfo.push(newNote)
         
         fs.writeFile('db/db.json', JSON.stringify(dbInfo), 'utf8', (err) => {
         if(err){ res.status(400).json(err) };
-        res.json(dbInfo);
+        return true;
         });
     });
 
