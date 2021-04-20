f277a54972a6cc1cafc34f438e3d124ca6d7f38b
mkotte <74936870+mkotte@users.noreply.github.com>
2021-04-19 23:03:56 -0400

    reverted app.delete changes
    will attempt later

--- server.js
+++ server.js
@@ -2,34 +2,36 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const db = require('db/db.json')
 
 // Express Configurations
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
-//
+// Route to /notes.html
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
-//
+
+// API get request to obtain notes from db.json
 app.get('/api/notes', (req, res) => {
-    fs.readFile(db, 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if(err){ res.status(400).json(err) }; 
 
         res.status(200).json(JSON.parse(data));
     });     
 });
 
-//
+// Default Route - has to be under all other app.get's
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+// Post Route to read then rewrite db.json, adding the new note
 app.post('/api/notes', (req, res) => {
     
-    fs.readFile(db, 'utf8', (err, data) => {
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
         if(err){ res.status(400).json(err) };
         const dbInfo = JSON.parse(data);
         const newNote = {
@@ -39,7 +41,7 @@ app.post('/api/notes', (req, res) => {
         };
         dbInfo.push(newNote)
         
-        fs.writeFile('db', JSON.stringify(dbInfo), 'utf8', (err) => {
+        fs.writeFile('db/db.json', JSON.stringify(dbInfo), 'utf8', (err) => {
         if(err){ res.status(400).json(err) };
         return true;
         });
@@ -48,10 +50,10 @@ app.post('/api/notes', (req, res) => {
     res.redirect('/')
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-    const noteIndex = db.find(({id}) => id === JSON.parse(req.params.body);
-    console.log(noteIndex)
-})
+// will attempt app.delete later.
+// app.delete('/api/notes/:id', (req, res) => {
+//     let trashedNote
+// })
 
-// listener 
+// Listener 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
